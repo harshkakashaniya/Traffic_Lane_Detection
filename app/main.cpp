06bdec32327e4940f527bd56dac6fffa793c7de9
harsh.kakashaniya <harsh.kakashniya@gmail.com>
2018-10-14 03:45:25 -0400

    Style:Doxygen comment and license add for main and test file

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
-#include <lib.hpp>
+/**
+ *  Copyright 2018 Harsh Kakashaniya
+ *  @file    Lane.cpp
+ *  @author  rohith517 (Navigator) and harshkakashaniya (driver)
+ *  @date    10/12/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 5,Midterm Project.
+ *
+ *  @Description DESCRIPTION
+ *
+ *  Class member functions for LaneDetectionModule.cpp
+ *
+ */
 
-int main()
-{
-    dummy();
-    return 0;
+#include "LaneDetectionModule.h"
+#include "Lane.h"
+#include <iostream>
+
+  int main() {
+  // TODO(harshkakashaniya) Call required classes and print required values.
+  return 0;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,18 @@
+/**
+ *  Copyright 2018 Harsh Kakashaniya
+ *  @file    Lane.cpp
+ *  @author  rohith517 (Navigator) and harshkakashaniya (driver)
+ *  @date    10/12/2018
+ *  @version 1.0
+ *
+ *  @brief UMD ENPM 808X, Week 5,Midterm Project.
+ *
+ *  @Description DESCRIPTION
+ *
+ *  Class member functions for main.cpp for test
+ *
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
