636c1ae4eab8f7bdbfedf59ad8a26d87d4c431ec
rohit517 <rohithkrishna517@gmail.com>
2018-10-16 22:11:44 -0400

    Chore:[Task:Dbg_2_01] Fix LaneDetectModuleTest merge conflict issue

--- test/LaneDetectionModuleTest.cpp
+++ test/LaneDetectionModuleTest.cpp
@@ -1,18 +1,14 @@
 /************************************************************************
  MIT License
-
  Copyright (c) 2018 Harsh Kakashaniya,Rohitkrishna Nambiar
-
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
-
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
-
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -36,14 +32,10 @@
  */
 
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 #include "LaneDetectionModule.hpp"
 #include "opencv2/opencv.hpp"
 
-using ::testing::AtLeast;
-using ::testing::Return;
-using ::testing::_;
-// Lane module object
+// LaneDetectionModule module object
 LaneDetectionModule laneModule;
 
 /**
@@ -132,44 +124,30 @@ TEST(GetSetTest, SetYellowMin) {
  *@param GetSetTest       Get set function test
  *@param SetYellowMax     Name of the unit test
  */
-/*TEST(GetSetTest, SetYellowMax) {
+TEST(GetSetTest, SetYellowMax) {
   laneModule.setYellowMax(cv::Scalar(150, 150, 150));
   cv::Scalar highTheshold = laneModule.getYellowMax();
   ASSERT_EQ(highTheshold, cv::Scalar(150, 150, 150));
 }
-/*
-// Google mock for other functions..
-class Mock_LD : public LaneDetectionModule {
-public:
-MOCK_METHOD2(undistortImage , void(const cv::Mat& src, cv::Mat& dst));
-};
 
-class MyLaneDetection {
-  LaneDetectionModule & LDM;
-public:
-  MyLaneDetection(LaneDetectionModule & LDM): LDM(_LDM);{}
+/**
+ *@brief Test for single image execution..
+ *
+ *@param FunctionalTest   Get set function test
+ *@param TestImage        Name of the unit test
+ */
+TEST(FunctionalTest, TestImage) {
+  bool status = laneModule.detectLane("../input/ColorImage.jpg");
+  ASSERT_EQ(status, true);
+}
 
-void undisto(_,_);
-};
-
-TEST(GetSetTest,undistortImage);
-//Arrange
-cv::Mat A,B;
-//float data1[2][2] = {{1,1},{1,1}};
-//float data2[2][2] = {{2,2},{2,2}};
-//A=Mat(2,2,CV_32FC1, &data1, 2);
-//B=Mat(2,2,CV_32FC1, &data2, 2);
-
-MOCK_LD MLD;
-LaneDetectionModule LaneDM(MLD);
-//ACT
-int undistort=LaneDM.undisto(A,B);
-//ASSERT
-ASSERT_EQ(undistort,0);
-*/
-
-TEST(FunctionTest, undistort) {
-
-  cv::Scalar dst = laneModule.();
-  ASSERT_EQ(lowTheshold, cv::Scalar(50, 50, 50));
+/**
+ *@brief Test for false image path.
+ *
+ *@param FunctionalTest     Get set function test
+ *@param TestFalseImagePath Name of the unit test
+ */
+TEST(FunctionalTest, TestFalseImagePath) {
+  bool status = laneModule.detectLane("../input/ColorImage1.jpg");
+  ASSERT_EQ(status, false);
 }
